3f8ce387b43d7b46dd4a1d7d38a414b3be7f73a8
chudaraj poudyal <crpoudyal@gmail.com>
2020-06-27 11:48:46 +0545

    [Added]buttton

--- lib/discount.dart
+++ lib/discount.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Discount extends StatefulWidget {
   @override
   _DiscountState createState() => _DiscountState();
@@ -10,52 +9,51 @@ class _DiscountState extends State<Discount> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text("Discount"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Center(
-          
-          child:Column(
+        appBar: AppBar(
+          title: Text("Discount"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Center(
+              child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            children:<Widget>[
-              
-             
+            children: <Widget>[
               TextField(
-                  keyboardType: TextInputType.number,
-                 
-                  decoration: InputDecoration(
-                    hintText: "Enter a number",
-                    labelText: "Total",
-                  ),
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Enter a number",
+                  labelText: "Total",
                 ),
-                 SizedBox(
-                  height: 10.0,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  hintText: "Enter a number",
+                  labelText: "Discount Percentage (%)",
                 ),
-                 TextField(
-                  keyboardType: TextInputType.number,
-                 
-                  decoration: InputDecoration(
-                    hintText: "Enter a number",
-                    labelText: "Discount Percentage (%)",
-                  ),
-                ),
-                 SizedBox(
-                  height: 50.0,
-                ),
-                Text("Result : ",
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RaisedButton(
+                onPressed: () {},
+                child: Text("GO"),
+              ),
+              SizedBox(
+                height: 50.0,
+              ),
+              Text(
+                "Result : ",
                 style: TextStyle(
                   fontSize: 30.0,
-                  fontWeight:FontWeight.bold,
+                  fontWeight: FontWeight.bold,
                 ),
-                ),
-               
+              ),
             ],
-          )
-        ),
-      )
-      
-    );
+          )),
+        ));
   }
 }
